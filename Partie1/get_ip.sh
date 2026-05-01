b1b7ff2c1a7d520f7a1c4fbdb14f5967dfb287c4
DesryLoic <loic.desry@student.junia.com>
2026-05-01 17:23:41 +0200

    Ajout de la VM de monitoring

--- Partie1/get_ip.sh
+++ Partie1/get_ip.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
-echo "Récupération de l'adresse IP depuis Terraform..."
+echo "Récupération des adresses IP depuis Terraform..."
 
-# extraction de l'ip brute
+# Extraction des IPs
 VM_IP=$(terraform output -raw vm_ip)
+MONITOR_IP=$(terraform output -raw monitor_ip)
 
-# vérification pour savoir si terraform a bien renvoyé quelque chose
-if [ -z "$VM_IP" ]; then
-    echo "Erreur : Impossible de récupérer l'IP. La VM est-elle bien démarrée ?"
+# Vérifications
+if [ -z "$VM_IP" ] || [ -z "$MONITOR_IP" ]; then
+    echo "Erreur : Impossible de récupérer les deux IPs. Vérifiez l'état de Terraform."
     exit 1
 fi
 
-echo "IP trouvée : $VM_IP"
+echo "IP K3s trouvée : $VM_IP"
+echo "IP Monitoring trouvée : $MONITOR_IP"
+
 echo "Génération du fichier inventory.ini..."
 
-# on écrit ou écrase le fichier inventory.ini avec les bonnes informations
+# On génère un inventaire complet avec les deux groupes
 cat <<EOF > inventory.ini
 [k3s_master]
 debian-k3s ansible_host=$VM_IP
 
-[k3s_master:vars]
+[monitoring]
+debian-monitor ansible_host=$MONITOR_IP
+
+[all:vars]
 ansible_user=vagrant
 ansible_password=vagrant
 ansible_ssh_common_args='-o StrictHostKeyChecking=no'
 EOF
 
-echo "Fichier inventory.ini généré avec succès !"
+echo "Fichier inventory.ini mis à jour avec succès !"
---
+++ Partie1/monitoring.sh
@@ -0,0 +1,16 @@
+resource "virtualbox_vm" "monitor_node" {
+  count     = 1
+  name      = "vm_tp_final_monitoring"
+  image     = "https://vagrantcloud.com/generic/boxes/debian11/versions/4.3.12/providers/virtualbox.box"
+  cpus      = 1
+  memory    = "1024 mib"
+
+  network_adapter {
+    type           = "bridged"
+    host_interface = var.network_host_if
+  }
+}
+
+output "monitor_ip" {
+  value = virtualbox_vm.monitor_node[0].network_adapter[0].ipv4_address
+}
