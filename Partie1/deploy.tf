91d9aa28bb686451d2b5df8f5709b83e795bc06b
DesryLoic <loic.desry@student.junia.com>
2026-05-02 12:35:33 +0200

    Fix: Remise en place de la version stricte alpha pour VirtualBox

--- Partie1/deploy.tf
+++ Partie1/deploy.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     virtualbox = {
       source  = "terra-farm/virtualbox"
+      version = "0.2.2-alpha.1"
     }
   }
 }
-
 
 variable "network_host_if" {
   description = "Nom de la carte reseau hote pour le bridge"
