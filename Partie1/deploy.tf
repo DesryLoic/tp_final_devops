5cf25187bdaedd8867d260a2d2e15432d916c57b
DesryLoic <loic.desry@student.junia.com>
2026-05-06 14:55:07 +0200

    Rajout de commentaires sur les documents

--- Partie1/deploy.tf
+++ Partie1/deploy.tf
@@ -10,9 +10,10 @@ terraform {
 variable "network_host_if" {
   description = "Nom de la carte reseau hote pour le bridge"
   type        = string
-  default     = "MediaTek Wi-Fi 6E MT7922 (RZ616) 160MHz PCIe Adapter"
+  default     = "MediaTek Wi-Fi 6E MT7922 (RZ616) 160MHz PCIe Adapter" # Modifier en fonction du PC
 }
 
+# Création de la machine virtuelle principale
 resource "virtualbox_vm" "debian_node" {
   count     = 1
   name      = "vm_tp_final"
@@ -21,11 +22,12 @@ resource "virtualbox_vm" "debian_node" {
   memory    = "2.0 gib"
 
   network_adapter {
-    type           = "bridged"
+    type           = "bridged" #  pour que la VM puisse avoir une IP sur le même réseau que le PC physique
     host_interface = var.network_host_if
   }
 }
 
+# Expose l'IP de la VM
 output "vm_ip" {
   value = virtualbox_vm.debian_node[0].network_adapter[0].ipv4_address
 }
--- Partie1/get_ip.sh
+++ Partie1/get_ip.sh
@@ -2,11 +2,14 @@
 
 echo "Récupération des adresses IP depuis Terraform..."
 
+# Récupère l'IP de la VM principale (K3s) générée par Terraform
 VM_IP=$(terraform output -raw vm_ip)
 
+# Change de dossier pour récupérer l'IP de la VM monitoring
 cd ../Partie5
 MONITOR_IP=$(terraform output -raw monitor_ip)
 
+# Sécurité : Stoppe la pipeline si l'une des IPs est introuvable
 if [ -z "$VM_IP" ] || [ -z "$MONITOR_IP" ]; then
     echo "Erreur : Impossible de récupérer les deux IPs. Vérifiez l'état de Terraform."
     exit 1
@@ -17,8 +20,9 @@ echo "IP Monitoring trouvée : $MONITOR_IP"
 
 echo "Génération du fichier inventory.ini..."
 
-# 3. On remonte à la racine du projet (..) pour créer le fichier d'inventaire
 cd ..
+
+# Génère le fichier inventory.ini avec les IPs dynamiques
 cat <<EOF > inventory.ini
 [k3s_master]
 debian-k3s ansible_host=$VM_IP
@@ -29,7 +33,7 @@ debian-monitor ansible_host=$MONITOR_IP
 [all:vars]
 ansible_user=vagrant
 ansible_password=vagrant
-ansible_ssh_common_args='-o StrictHostKeyChecking=no'
+ansible_ssh_common_args='-o StrictHostKeyChecking=no' # Désactive la confirmation manuelle (yes/no) de la clé SSH
 EOF
 
 echo "Fichier inventory.ini mis à jour avec succès à la racine du projet !"
